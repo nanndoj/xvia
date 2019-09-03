b6008ff1bdccf1564e3a9f5d72cf775d8e00b21f
fernandosantos <fernando.santos@king.com>
2019-09-03 13:49:27 +0200

    Fixed login page layout

--- src/common-ui/src/main/java/ee/ria/xroad/commonui/jaas/AuthFilter.java
+++ src/common-ui/src/main/java/ee/ria/xroad/commonui/jaas/AuthFilter.java
@@ -66,7 +66,7 @@ public class AuthFilter implements Filter {
                 && !httpRequest.getRequestURI().startsWith(ctx + "/stylesheets/")
                 && !httpRequest.getRequestURI().equals(ctx + "/application/skin")
                 && !httpRequest.getRequestURI().equals(ctx + "/xvia-favicon.ico")
-                && !httpRequest.getRequestURI().equals(ctx + "/xvia-logo-large.png")
+                && !httpRequest.getRequestURI().endsWith(".png")
                 // Finally check authentication.
                 && httpRequest.getUserPrincipal() == null
                 && !httpRequest.authenticate(httpResponse)) {
