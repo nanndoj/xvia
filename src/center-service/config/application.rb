e812b53a1496f4d0dc5cfbef299033a5997e1077
fernandosantos <fernando.santos@king.com>
2019-09-04 11:12:20 +0200

    Proxy ui translations

--- src/center-service/config/application.rb
+++ src/center-service/config/application.rb
@@ -58,7 +58,9 @@ module CenterService
 
     # The default locale is :et and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
