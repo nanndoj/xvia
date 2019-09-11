3b161e9e6a3a2f23de6f6e617515c721f19387be
fernandosantos <fernando.santos@king.com>
2019-09-11 15:47:18 +0200

    Partial: RPM installer

---
+++ src/packages/src/xroad/installer/redhat/install-centralserver.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# OS: Ubuntu
+# Module: Central Server
+
+DIRNAME=$(cd `dirname $0` && pwd)
+
+sudo apt-get --assume-yes install nginx-light
+sudo apt-get --assume-yes install postgresql
+sudo apt-get --assume-yes install postgresql-contrib
+sudo dpkg -i $DIRNAME/xroad-base_*.deb
+sudo apt-get --assume-yes install -f
+sudo dpkg -i $DIRNAME/xroad-nginx_*.deb
+sudo dpkg -i $DIRNAME/xroad-confclient_*.deb
+sudo dpkg -i $DIRNAME/xroad-jetty9_*.deb
+sudo dpkg -i $DIRNAME/xroad-signer_*.deb
+
+#Install X-Road Center
+sudo dpkg -i $DIRNAME/xroad-center_*.deb
+sudo dpkg -i $DIRNAME/xroad-centralserver-monitoring_*.deb
+sudo apt-get --assume-yes install -f
